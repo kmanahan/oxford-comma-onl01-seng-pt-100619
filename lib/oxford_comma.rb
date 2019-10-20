32678f1757225b3cf3d05e95c5d07a387d30f76e
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-10-20 17:26:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2 
     array.join(" and ")
   else 
-    array[-1].insert(0, " and ")
+    array[-1].insert(0, "and ")
   end 
   return array.join(", ")
   
